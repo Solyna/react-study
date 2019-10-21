31315009b10bd93823b4f83a6862a97193dc6fc3
songling <18785019121@sina.cn>
2019-10-21 15:17:25 +0800

    路由的标签式跳转和编程式跳转的使用

---
+++ next-create/pages/RouterA.js
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+
+export default ()=>(
+    <>
+        <div>标签式导航A页面</div>
+        {/* ！！！注意这里有个坑：他不支持兄弟标签并列的情况 */}
+        {/*  <Link href="/jspangA">
+            <span>去JspangA页面</span>
+            <span>前端博客</span>
+        </Link> */}
+        {/* 可以把这两个标签外边套一个父标签 */}
+        {/* <Link href="/jspangA">
+            <a>
+                <span>去JspangA页面</span>
+                <span>前端博客</span>
+            </a>
+        </Link> */}
+        <Link href="/">
+            <a>返回首页</a>
+        </Link>
+    </>
+)
---
+++ next-create/pages/RouterB.js
@@ -0,0 +1,8 @@
+import Link from 'next/link';
+
+export default ()=>(
+    <>
+        <div>标签式导航B页面</div>
+        <Link href="/"><a>返回首页</a></Link>
+    </>
+)
