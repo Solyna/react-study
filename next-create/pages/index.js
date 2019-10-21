fc8c6aca052bc2b027fdba265fb408f9e0c9a56a
songling <18785019121@sina.cn>
2019-10-21 16:05:18 +0800

    p06：路由-六个钩子事件

--- next-create/pages/index.js
+++ next-create/pages/index.js
@@ -5,6 +5,38 @@ import Link from 'next/link'
 import Router from 'next/router'
 
 const Home=()=>{
+/* 六个钩子事件 */
+/* routeChangeStart */
+/* routeChangeComplete */
+/* beforeHistoryChange */
+/* routeChangeError */
+/* hashChangeStart */
+/* hashChangeComplete */
+
+  Router.events.on('routeChangeStart',(...args)=>{
+    console.log('1.routeChangeStart->路由开始变化，参数为:',...args)
+  })
+
+  Router.events.on('routeChangeComplete',(...args)=>{
+    console.log('2.routeChangeComplete->路由变化结束，参数为:',...args)
+  })
+
+  Router.events.on('beforeHistoryChange',(...args)=>{
+    console.log('3.beforeHistoryChange->路由变化结束，参数为:',...args)
+  })
+  /* 404的情况无法监听 */
+  Router.events.on('routeChangeError',(...args)=>{
+    console.log('4.routeChangeError->路由发生错误，参数为:',...args)
+  })
+  Router.events.on('hashChangeStart',(...args)=>{
+    console.log('5.hashChangeStart->哈希路由切换之前，参数为:',...args)
+  })
+  Router.events.on('hashChangeComplete',(...args)=>{
+    console.log('6.hashChangeComplete->哈希路由转变之后，参数为:',...args)
+  })
+
+
+
   function gotoB(){
    /*  Router.push('/routerB?name=编程式传参参数B') */
    Router.push({
@@ -20,6 +52,9 @@ const Home=()=>{
       <div><Link href="/routerB?name=参数b"><a>去RouterB页面</a></Link></div>
       <div>
         <button onClick={gotoB}>编程式导航跳转到A页面</button>
+      </div>
+      <div>
+        <Link href="#songling"><a>选择宋00</a></Link>
       </div>
     </>
   )
